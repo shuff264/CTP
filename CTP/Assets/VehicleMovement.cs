dc145372090d72dba8a54609a013de4da7acd220
shuff264 <jshuff264@gmail.com>
2016-01-03 22:25:06 +0000

    Basic position Lerp

--- CTP/Assets/VehicleMovement.cs
+++ CTP/Assets/VehicleMovement.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class VehicleMovement : MonoBehaviour {
 
+	Vector3 startPosition;
+	public Vector3 endPosition;
+	float speed = 1.0f;
+
+	float startTime;
+	float journeyLength;
+
 	// Use this for initialization
 	void Start () {
+
+		float randX = Random.Range (0, 100);
+		float randY = Random.Range (0, 100);
+
+		startTime = Time.time;
+		startPosition = gameObject.transform.position;
+		endPosition = new Vector3 (randX, 0, randY);
+		journeyLength = Vector3.Distance (startPosition, endPosition);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		gameObject.transform.position = Vector3.Lerp (startPosition, endPosition, fracJourney);
 	
 	}
 }
--- CTP/Assets/VehicleSpawn.cs
+++ CTP/Assets/VehicleSpawn.cs
@@ -10,6 +10,8 @@ public class VehicleSpawn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+			
 	
 	}
 	
