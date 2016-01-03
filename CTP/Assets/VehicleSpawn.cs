eecc0be2f7b35db7b2ea71a72f726d0afe0e548a
shuff264 <jshuff264@gmail.com>
2016-01-03 15:37:25 +0000

    Spawning
    Added the basic script for spawning of vehicles

---
+++ CTP/Assets/VehicleSpawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawn : MonoBehaviour {
+
+	int randomValue;
+	int randomX;
+	int randomY;
+	public GameObject car;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		randomValue = Random.Range (0, 100);
+		randomX = Random.Range (0, 100);
+		randomY = Random.Range (0, 100);
+		
+
+		if (randomValue < 50) {
+		
+			Instantiate(car, new Vector3(randomX, 0, randomY), Quaternion.identity);
+		
+		}
+
+
+	}
+}
