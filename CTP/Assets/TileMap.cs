4a6cdc88a7201e8a82bd9ea83ef544c864b2f785
shuff264 <jshuff264@gmail.com>
2016-01-06 14:01:42 +0000

    Added the basis of a tile map
    Creates a 10x10 grid of a temp prefab object

---
+++ CTP/Assets/TileMap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap : MonoBehaviour {
+
+	public TileType[] tileTypes;
+
+	int[,] tiles;
+
+	int mapSizeX = 10;
+	int mapSizeY = 10;
+
+	void Start() {
+
+		tiles = new int[mapSizeX, mapSizeY];
+
+		for(int x = 0; x < mapSizeX; x++){
+			for(int y = 0; y < mapSizeY; y++){
+				tiles[x,y] = 0;
+			}
+		}
+
+		for (int x = 0; x < mapSizeX; x++) {
+			for (int y = 0; y < mapSizeY; y++) {
+				TileType tt = tileTypes[tiles[x,y]];
+
+				Instantiate(tt.tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
+			}
+		}
+	}
+
+}
