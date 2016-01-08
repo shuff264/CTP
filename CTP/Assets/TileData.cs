483ed745501ee1a41cfc4169d134f9a4c83a134f
shuff264 <jshuff264@gmail.com>
2016-01-08 14:14:01 +0000

    TileData base class
    Will be used to store data about which tiles are next to each other

---
+++ CTP/Assets/TileData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileData {
+
+	// Needs info of the four tiles it is next to
+	// This can then be used to create a map
+	// For a search algorithm to use
+	// Might just need the number, might need the actual game object?
+	// Will have to know about the tile map info
+
+}
